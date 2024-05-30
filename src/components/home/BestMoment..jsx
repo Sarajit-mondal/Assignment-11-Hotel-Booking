0e2142e85bd9bf497512873a78b640c5e4f69abc
sarajit <bdsarajit499@gamil.com>
2024-05-30 07:46:25 +0600

    imageBB upload complete

--- src/components/home/BestMoment..jsx
+++ src/components/home/BestMoment..jsx
@@ -31,10 +31,10 @@ const imageUpload = async() =>{
     const formData = new FormData();
     formData.append('image',image)
 
-   const {data} =await axios.post(`https://api.imgbb.com/1/upload/key=${import.meta.env.VITE_IMAGEBB_API_KEY}`,
+   const {data} =await axios.post(`https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMAGEBB_API_KEY}`,
    formData
    )
-   console.log(data)
+   console.log(data.data.display_url)
 
   }
  } catch (error) {
